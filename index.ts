eb9d0c1cf91c4b5b2105393c90603a4098200428
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-18 20:52:28 +0700

    Update github link

--- index.ts
+++ index.ts
@@ -43,7 +43,7 @@ async function main() {
   let distances = chistmasday - now;
   let chistmass = Math.floor(distances / (1000 * 60 * 60 * 24));
 
-  let ds = `2023 is in ${newyears} days 🎆 \nChristmas is in ${chistmass} days 🎄\n\nProfile will auto update every 40 minutes https://github.com/ronnapatp/twitter-bot`
+  let ds = `2023 is in ${newyears} days 🎆 \nChristmas is in ${chistmass} days 🎄\n\nProfile will auto update every 40 minutes https://ronnapat.com/tw-bot`
 
   const ans = await grabGithubData();
   const params = {
