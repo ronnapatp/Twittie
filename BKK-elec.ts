ee59eea2c600817f8acb8101eb4f365f6b4e8adb
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-05-06 19:51:10 +0700

    add bkk elec

---
+++ BKK-elec.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+import Twitter from 'twitter';
+
+dotenv.config()
+
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY as string,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string
+});
+
+
+async function main() {
+
+  let date = new Date(Date.UTC(2021, 5, 28, 3, 0, 0));
+  let formatter = new Intl.DateTimeFormat('en-US', { timeZone: "Asia/Bangkok" });   
+  let thDate = formatter.format(date);
+  const dates = thDate.split("0")
+  const datesFormat = dates[0]
+  parseFloat(datesFormat)
+  let elecday = 22
+  let daystoelec = elecday - parseFloat(datesFormat)
+  let days;
+
+  if( daystoelec == 1 ){
+    days = "📣 Tomorrow is Bangkok election day"
+  } else if ( daystoelec == 0) {
+    days = "📣 Today is Bangkok election day"
+  } else {
+    days = `📣 Bangkok election in ${daystoelec} days`
+  }
+
+
+  const tweet = {
+    status: days
+  }
+
+  await client.post("statuses/update", tweet);
+    console.log("🎉 Success! Updated Twitter statuses/update");
+}
+main().catch(err=> console.log(err))
