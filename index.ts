ff82385387d4c429c5853d1cf541bcce9ec805da
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-05-23 10:10:44 +0700

    update bio

--- index.ts
+++ index.ts
@@ -39,7 +39,7 @@ async function main() {
   let newyears = Math.floor(distance / (1000 * 60 * 60 * 24));
   const githubData = await grabGithubData();
 
-  await client.v1.updateAccountProfile({ name: 'ronnapatp 🇺🇦', url: 'https://ronnapat.com/', description: `#SlavaUkraine 🇺🇦`, location: githubData })
+  await client.v1.updateAccountProfile({ name: 'ronnapatp 🇺🇦', url: 'https://ronnapat.com/', description: `#SlavaUkraine 🇺🇦 | Politics Enthusiast`, location: 'Thailand' })
   console.log('Done!')
 }
 main().catch(err=> console.log(err))
